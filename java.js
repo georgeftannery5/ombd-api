b31ee95b6f5e611d33410dc48f9378e6edcec67f
George Tannery <geetv13@gmail.com>
2024-08-17 16:18:54 -0400

    api add

--- java.js
+++ java.js
@@ -1,25 +1,45 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=d7dc8cb0&s=star
 // http://img.omdbapi.com/?apikey=[d7dc8cb0]&
 
+function openMenu(){
+    document.body.classList += "menu--open"
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu--open')
+}
+
 const moviesListEl = document.querySelector('.movie-list');
 
 async function main() {
     const movies = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=d7dc8cb0&s=star");
     const moviesData = await movies.json();
-    moviesListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
+    moviesListEl.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
 }
 
-
-main(); 
+main();
 
 function movieHTML(movie) {
     return `<div class="movie-card">
                     <div class="movie-card__container">
-                            <h3>${movie.title}</h3>
-                            <p><b>Year</b>${movie.year}</p>
-                            <p><b>imdbID</b>${movie.imdbID}}</p>
-                            <p><b>Type</b>${movie.type}</p>
-                            <p><b>Poster</b>${movie.poster}</p>
+                            <h3>${movie.Title}</h3>
+                            <p><b>Year:</b>${movie.Year}</p>
+                            <p><b>imdbID:</b>${movie.imdbID}}</p>
+                            <p><b>Type:</b>${movie.Type}</p>
+                            <p><b>Poster</b>${movie.Poster}</p>
                     </div>
                 </div>`;
+}
+
+function movieHTML(movie) {
+    return `<div class="movie"">
+            <a class="movie-card__container"
+                ><img
+                class="movie__poster"
+                src="${movie.Poster}"
+                alt=""
+            ></a>
+            <h3><b>${movie.Title}</b></h3>
+            <p>${movie.Year}</p>
+        </div>`;
 }
